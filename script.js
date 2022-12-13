7a5ed01e542b60f56442d0b17309062ae46b528f
Méline Pischedda <98113220+meline-p@users.noreply.github.com>
2022-12-13 02:31:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,300 @@
+//TMDB
+
+const API_KEY = 'api_key=dc3faa9383bbe7ed2606a612df2b9370';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+
+const genres = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const tagsEl = document.getElementById('tags');
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const current = document.getElementById('current');
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+var selectedGenre = [];
+
+setGenre();
+
+// METTRE DES GENRES
+function setGenre(){
+    tagsEl.innerHTML = ``;
+
+    // AFFICHER LES GENRES
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag');
+        t.id = genre.id;
+        t.innerText = genre.name;
+
+        t.addEventListener('click', () =>{
+            if(selectedGenre.length == 0){ // si aucun genre n'est sélectionné
+                selectedGenre.push(genre.id); //ajouter l'id du genre
+            } else{ // sinon
+                if(selectedGenre.includes(genre.id)){ // si un genre est sélctionné
+                    selectedGenre.forEach((id, idx) => { // pour chaque genre sélectionné avec un id et un index
+                        if(id == genre.id){ // si l'id = à l'id du genre
+                            selectedGenre.splice(idx, 1); //modifier l'array pour ne garder que les movies qui ont cet id 
+                            //supprime 1 élément à partir de l'index 'idx'
+                        }
+                    })
+                }else{
+                    selectedGenre.push(genre.id);//ajouter l'id du genre
+                }
+            }
+            // obtenir les films avec les genres sélectionnés
+            getMovies(API_URL + '&with_genres=' + encodeURI(selectedGenre.join(',')));
+            //highlight les genres sélectionnés
+            highlightSelection();
+        })
+        // ajouter t à aux tags
+        tagsEl.append(t);
+    });
+};
+
+// HIGHLIGHT LES GENRES SELECTIONNES 
+function highlightSelection(){
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag => {
+        tag.classList.remove('highlight');
+    })
+    clearBtn();
+    if(selectedGenre.length != 0){
+        selectedGenre.forEach(id => {
+            const highlightTag = document.getElementById(id);
+            highlightTag.classList.add('highlight');
+        })
+    }
+    
+}
+
+// CLEAR BUTTON POUR LES GENRES
+function clearBtn(){
+    let clearBtn = document.getElementById('clear');
+    if(clearBtn){
+        clearBtn.classList.add('highlight');
+    }else{
+        let clear = document.createElement('div');
+        clear.classList.add('tag', 'highlight');
+        clear.id = 'clear';
+        clear.innerText = "Clear x"
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenre();
+            getMovies(API_URL);
+        })
+        tagsEl.append(clear);
+    }  
+}
+
+getMovies(API_URL);
+
+// OBTENIR LES FILMS
+function getMovies(url){
+    lastUrl = url;
+
+    // DONNEES API
+    fetch(url).then(res => res.json()).then(data => {
+        if(data.results.length !== 0){
+            showMovies(data.results);
+
+            // PAGINATION
+            currentPage = data.page;
+            nextPage = currentPage + 1;
+            prevPage = currentPage - 1;
+            totalPages = data.total_pages;
+
+            current.innerText = currentPage;
+
+            if(currentPage <= 1){
+                prev.classList.add('disabled');
+                next.classList.remove('disabled');
+            } else if(currentPage >= totalPages) {
+                prev.classList.remove('disabled');
+                next.classList.add('disabled');
+            } else {
+                prev.classList.remove('disabled');
+                next.classList.remove('disabled');
+            }
+
+            // SCROLL VERS LE HAUT
+            tagsEl.scrollIntoView({behavior : "smooth"});
+
+        // NO RESULTS
+        } else{
+            main.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+        }
+    });
+}
+
+// MONTRER LES DONNEES DU FILM
+function showMovies(data){
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img 
+            src="${poster_path? IMG_URL + poster_path : "http://via.placeholder.com/1080x1580"}" 
+            alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h4>Overview:</h4>
+            ${overview}
+        </div>
+        `
+        main.appendChild(movieEl);
+    });
+}
+
+// NOTE DU FILM
+function getColor(vote){
+    if(vote >= 8){
+        return 'green'
+    } else if(vote >= 5){
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+
+//SEARCH BUTTON
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    selectedGenre = [];
+    setGenre();
+
+    if(searchTerm){
+        getMovies(searchURL + '&query=' + searchTerm);
+    }else {
+        getMovies(API_URL);
+    };
+
+});
+
+//NEXT PAGE
+next.addEventListener('click', () =>{
+    if(nextPage <= totalPages){
+        pageCall(nextPage);
+    }
+});
+//PREV PAGE
+prev.addEventListener('click', () =>{
+    if(prevPage > 0){
+        pageCall(prevPage);
+    }
+});
+
+function pageCall(page){
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length -1].split("=");
+    if(key[0] != 'page'){
+        let url = lastUrl + "&page=" + page;
+        getMovies(url);
+    } else{
+        key[1] = page.toString();
+        let a = key.join('='); // "=" numéro page
+        queryParams[queryParams.length -1] = a; //dernier élément de queryParams = numéro page
+        let b = queryParams.join('&'); // "&"clé API 
+        let url = urlSplit[0] + '?' + b; // url + "?"" + queryParams (+numéro page)
+
+        getMovies(url);
+    }
+}
